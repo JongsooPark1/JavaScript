e5d41972fc13a27f3c6d11a26a87f807d47019ee
= <rightgear11@gmail.com>
2017-10-22 03:05:44 +0900

    JavaScript

--- Project/readme.md
+++ Project/readme.md
@@ -1,3 +1,5 @@
 ## JavaScript
 ---
 * [러시안룰렛](https://github.com/JongsooPark1/JavaScript/tree/master/Project/%EB%9F%AC%EC%8B%9C%EC%95%88%EB%A3%B0%EB%A0%9B)
+
+* [별찍기]()
---
+++ Project/러시안룰렛/러시안룰렛.js
@@ -0,0 +1,18 @@
+alert("지금부터 러시안룰렛 게임을 시작하겠습니다");
+var bulletNum = Math.ceil(Math.random() * 6);
+console.log(bulletNum);
+var name1 = prompt("첫번째로 방아쇠를 당기는 사람의 이름을 입력하세요");
+var name2 = prompt("두번째로 방아쇠를 당기는 사람의 이름을 입력하세요");
+
+for (var i = 1; i <= 6; i++) {
+  if (i === bulletNum) {
+    alert(i+"번째 방아쇠를 당기세요");
+    if (i % 2 === 1) {
+      alert(name1 + "님이 사망하였습니다 ㅠㅜ");
+    } else {
+      alert(name2 + "님이 사망하였습니다 ㅠㅜ");
+    }
+    break;
+  }
+  alert(i+"번째 방아쇠를 당기세요");
+}
