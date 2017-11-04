cea578c440dd20154c358c9eff6c1d0b798fbb32
= <rightgear11@gmail.com>
2017-11-04 12:34:10 +0900

    modify animation.js in animation folder

--- proj/animation/animation.js
+++ proj/animation/animation.js
@@ -5,26 +5,18 @@ var ctx = canvas.getContext("2d");
 // 이미지 불러오기
 var bgImage = new Image();
 var bgload = false;
-// bgImage.onload = function () {    // Async, 원래는 이미지를 가져오는데 시간이 걸린다. 그래서 쉬운 작업인 공을 그리는 것부터 시작하고,
-//   bgload = true;                 // 배경을 그리기 때문에, 그린 공이 노출되지 않는다. 따라서 이와같이 코드를 변경하여 배경을 먼저 그린 뒤,
-// };                                // 공을 그리게 한다.
-bgImage.src = "supermario.jpg";
-
-//draw background image
-// var drawBackground = function() {
-//   ctx.drawImage(bgImage, 300, 0); //전체 이미지에서 x좌표는 300부터, y좌표는 0부터만 보여주기
-// }
-
-var bgImage = new Image();
-
+bgImage.onload = function() {
+  alert("onload");
+  bgload = true;
+};
 bgImage.src = "supermario.jpg";
 
 //ball 객체 만들기
 var ball = {
   x: 10,
   y: 300,
-  dx: 10000,
-  dy: 10000,
+  dx: 100,
+  dy: 100,
   radius: 10,
   color: "red",
   move: function() {
@@ -37,12 +29,10 @@ var ball = {
   },
   check: function() {
       if (this.x < 10 || this.x > 1014) {
-        // this.dx = 0;
         this.dx = -1 * (1.2 * this.dx);
       }
 
       if (this.y < 10 || this.y > 630) {
-        // this.dy = 0;
         this.dy = -1 * (1.2 * this.dy);
     }
   }
@@ -50,6 +40,7 @@ var ball = {
 
 //공 그리기
 ctx.drawBall = function() {
+  alert("drawBall");
   this.beginPath();
   this.fillStyle = ball.color;
   this.ellipse(ball.x, ball.y, ball.radius, ball.radius,
@@ -61,15 +52,15 @@ ctx.drawBall = function() {
 ball.setSpeed(5, -1);
 
 var drawAll = function() {
-  // if (bgload) {
-  //   drawBackground();
-  // }
-  // ctx.drawImage(bgImage, 0, 0);
-  ctx.drawImage(bgImage, 0, 0);
-
+  if (bgload) {
+    alert("drawBackground");
+     ctx.drawImage(bgImage, 0, 0);
+  }
   ball.check();
   ball.move();
   ctx.drawBall();
 };
 
 setInterval(drawAll, 300);
+// drawAll();
+// ctx.drawBall();
