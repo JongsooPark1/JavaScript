b2dbb7ad28481fcc597a071eafdeddec9873e7ea
= <rightgear11@gmail.com>
2018-02-01 14:57:15 +0900

    Modify

--- proj/sort2/code.js
+++ proj/sort2/code.js
@@ -35,7 +35,7 @@ function makeSort2(arr) {
 // 1. bubbleSort
 function bubbleSort(arr) {
   for (var i = 0; i < arr.length; i++) {
-    for (var j = 0; j < arr.length; j++) {
+    for (var j = 0; j < arr.length - 1; j++) {
       if (arr[j] > arr[j + 1]) {
         var temp = arr[j + 1];
         arr[j + 1] = arr[j];
